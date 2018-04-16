681780ebb5d8ff3f307d0436da37c134ce5778b6
DOITTEAU Loïc <DOITTEAU@dev.groupe.adg>
2018-04-16 23:47:17 +0200

    Genetic algorithm implementation

--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
+const SPEED = 100;
+
 const XMIN = -10;
 const XMAX = 10;
-const YMIN = -10;
+const YMIN = -100;
 const YMAX = 100;
 
 const WINDOW_WIDTH = 768;
@@ -9,9 +11,15 @@ const WINDOW_HEIGHT = 727;
 let SCREEN_X0;
 let SCREEN_Y0;
 
-let POINTS;
+let POINTS = [];
 
-let COEFS = [0, 1, 0, -9];
+let MIN_COEF = -100;
+let MAX_COEF = 100;
+let COEF_COUNT = 4
+let COEFS = [];
+
+let MOVE_MUTATION_RATE = 1 / 10;
+let REPLACE_MUTATION_RATE = 1 / 50;
 
 function setup()
 {
@@ -19,13 +27,12 @@ function setup()
     let ps = wordlToScreen(0, 0);
     SCREEN_X0 = ps.x;
     SCREEN_Y0 = ps.y;
-    POINTS = [];
+    for(let j = 0; j < COEF_COUNT; j++) {
+        COEFS.push(random(MIN_COEF, MAX_COEF));
+    }
     for(let x = XMIN; x <= XMAX; x++)
     {
-        let y = COEFS[0] * x * x * x +
-                COEFS[1] * x * x +
-                COEFS[2] * x +
-                COEFS[3];
+        let y = 0.5 * x * x * x + x * x - 9;
         POINTS.push({x, y});
     }
 }
@@ -54,10 +61,10 @@ function draw()
     for(let i = 0; i < WINDOW_WIDTH; i++)
     {
         let pw = screenToWorld(i, 0);
-        pw.y =  COEFS[0] * pw.x * pw.x * pw.x +
-                COEFS[1] * pw.x * pw.x +
-                COEFS[2] * pw.x +
-                COEFS[3];
+        pw.y = 0;
+        for(let j = 0; j < COEF_COUNT; j++) {
+            pw.y += COEFS[j] * pow(pw.x, COEF_COUNT - j - 1);
+        }
         let ps = wordlToScreen(pw.x, pw.y);
         vertex(ps.x, ps.y);
     }
@@ -70,6 +77,14 @@ function draw()
         let pw = POINTS[i];
         let ps = wordlToScreen(pw.x, pw.y);
         point(ps.x, ps.y);
+    }
+    for(let i = 0; i < COEF_COUNT; i++)
+    {
+        text(COEFS[i], 20, 20 * (i + 1));
+    }
+    for(let i = 0; i < SPEED; i++)
+    {
+        while(!mutate());
     }
 }
 
@@ -85,4 +100,42 @@ function screenToWorld(x, y)
     x = XMIN + x / WINDOW_WIDTH * (XMAX - XMIN);
     y = -(YMIN + (y - WINDOW_HEIGHT) / WINDOW_HEIGHT * (YMAX - YMIN));
     return {x, y};
+}
+
+function getFitness()
+{
+    let fitness = 0;
+    for(let i = 0; i < POINTS.length; i++)
+    {
+        let pw = POINTS[i];
+        let y = 0;
+        for(let j = 0; j < COEF_COUNT; j++) {
+            y += COEFS[j] * pow(pw.x, COEF_COUNT - j - 1);
+        }
+        fitness += abs(POINTS[i].y - y);
+    }
+    return fitness;
+}
+
+function mutate()
+{
+    let old_coefs = COEFS.slice();
+    let old_Fitness = getFitness();
+    var flag = false;
+    for(let i = 0; i < COEF_COUNT; i++)
+    {
+        if(random() < MOVE_MUTATION_RATE)
+        {
+            COEFS[i] += random(-1, 1);
+            flag = true;
+
+        }
+        if(random() < REPLACE_MUTATION_RATE)
+        {
+            COEFS[i] = random(MIN_COEF, MAX_COEF);
+            flag = true;
+        }
+    }
+    if(old_Fitness < getFitness()) COEFS = old_coefs;
+    return flag;
 }
